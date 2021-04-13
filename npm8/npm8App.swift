4ed0a94e8344ef6274b2dac612c1fdc95056df2b
Morris Albers <dagger5@alberslegal.com>
2021-04-12 21:10:45 -0500

    Initial Commit

---
+++ npm8/npm8App.swift
@@ -0,0 +1,17 @@
+//
+//  npm8App.swift
+//  npm8
+//
+//  Created by Morris Albers on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct npm8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
